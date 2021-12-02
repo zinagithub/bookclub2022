4abfd3dd9ce43686ab13d098368b1e9b8808bffd
Talaa Zina <talazina@gmail.com>
2021-12-02 09:50:42 +0100

    set up ejs engine

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const {TextDecoder, TextEncoder} = require("util");
-
 const express = require('express');
-//Logging
-if (process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-}
+
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const path = require('path');
 const connectDB = require('./config/db');
 
 //local config
 dotenv.config({path:'./config/config.env'});
 
 connectDB();
+
 const app = express();
+
+//Logging
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+//set the ejs engine
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
