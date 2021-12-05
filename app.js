2c8bf82b62f97f60b22e3bd3442020f94e01040c
Talaa Zina <talazina@gmail.com>
2021-12-05 18:37:53 +0100

    add authentication using express-openid-connect

--- app.js
+++ app.js
@@ -4,13 +4,38 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path = require('path');
 const connectDB = require('./config/db');
+const { auth, requiresAuth } = require('express-openid-connect');
 
 //local config
 dotenv.config({path:'./config/config.env'});
 
+const app = express();
+
+//authentication
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    secret: process.env.SECRET
+  };
+  
+  // auth router attaches /login, /logout, and /callback routes to the baseURL
+  app.use(auth(config));
+  
+  // req.isAuthenticated is provided from the auth router
+  app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+  });
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
 connectDB();
 
-const app = express();
+
 
 //Logging
 if (process.env.NODE_ENV === 'development'){
