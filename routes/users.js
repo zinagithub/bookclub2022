fdabef3cc447e0c37c30b80d54149cf919179c8a
Talaa Zina <talazina@gmail.com>
2021-12-14 14:05:19 +0100

    updates in routes, controllers and views

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv');
 const path = require('path');
 const connectDB = require('../config/db');
 const User = require('../models/user');
+const Book = require('../models/book');
+const Genre = require('../models/genre');
+var async = require('async');
 const { auth, requiresAuth } = require('express-openid-connect');
 const router = express.Router();
 
@@ -52,9 +55,32 @@ const config = {
         res.locals.user = null;
     }
     
-    console.log(res.locals.user);
-    res.render("index");
+    async.parallel({
+        genre: function(callback) {
+            Genre.find({})
+              .exec(callback);
+        },
+
+        books: function(callback) {
+            Book.find({})
+              .exec(callback);
+        },
+
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('index', {genre: results.genre, books: results.books});
+    });
+
   });
+
+
+
   router.get('/profile', requiresAuth(), (req, res) => {
     res.send(JSON.stringify(req.oidc.user));
   });  
