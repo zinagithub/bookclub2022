4c5de440c162374640c1dd45ac1cb86335f45ee6
Talaa Zina <talazina@gmail.com>
2021-12-20 15:59:00 +0100

    create genre controller-form

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,7 @@
 const Book = require('../models/book');
 const BookInstance = require('../models/bookinstance');
 var async = require('async');
+
 exports.index = function(req, res) {
     res.send('NOT IMPLEMENTED: Site Home Page');
 };
